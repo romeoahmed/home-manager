cfc7c80be30ddac8bb7102f6c6ca844257be6801
romeoahmed <ahmedorqwn@gmail.com>
2025-12-29 17:26:00 +0800

    styles: format by nixfmt

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,14 +1,14 @@
 { pkgs, ... }:
 
 {
-    home.packages = with pkgs; [
-      noto-fonts
-      noto-fonts-cjk-sans
-      noto-fonts-cjk-serif
-      noto-fonts-color-emoji
-      nerd-fonts.jetbrains-mono
-      inter
-    ];
+  home.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-color-emoji
+    nerd-fonts.jetbrains-mono
+    inter
+  ];
 
   fonts.fontconfig = {
     enable = true;
@@ -17,10 +17,20 @@
     subpixelRendering = "rgb";
 
     defaultFonts = {
-        sansSerif = [ "Inter" "Noto Sans" "Noto Sans CJK SC" ];
-        serif = [ "Noto Serif" "Noto Serif CJK SC" ];
-        monospace = [ "JetBrainsMonoNL Nerd Font" "Noto Sans Mono CJK SC" ];
-        emoji = [ "Noto Color Emoji" ];
+      sansSerif = [
+        "Inter"
+        "Noto Sans"
+        "Noto Sans CJK SC"
+      ];
+      serif = [
+        "Noto Serif"
+        "Noto Serif CJK SC"
+      ];
+      monospace = [
+        "JetBrainsMonoNL Nerd Font"
+        "Noto Sans Mono CJK SC"
+      ];
+      emoji = [ "Noto Color Emoji" ];
     };
   };
 }

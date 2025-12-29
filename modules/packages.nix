d8cd521a21287b51e02ef929b9e9cfacb1c3f402
romeoahmed <ahmedorqwn@gmail.com>
2025-12-29 14:48:37 +0800

    feat: add packages
    - cmakeMinimal
    - ninja
    - most
    - unzip
    - wl-clipboard
    - pv
    - htop

--- modules/packages.nix
+++ modules/packages.nix
@@ -5,18 +5,27 @@
     curl
     wget
     aria2
+    unzip
+    wl-clipboard
 
     nil
     nixfmt-rfc-style
 
     vim
     nano
+    most
 
     ripgrep
     fd
     bat
     eza
 
+    pv
+
+    cmakeMinimal
+    ninja
+
+    htop
     btop
     fastfetch
 

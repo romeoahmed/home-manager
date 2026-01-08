bd5f020a1fdcb6333bee134d9ca91c02c3ff328f
romeoahmed <ahmedorqwn@gmail.com>
2026-01-08 17:54:17 +0800

    chore: add oxalica cachix

--- configuration.nix
+++ configuration.nix
@@ -18,15 +18,11 @@
       substituters = [
         "https://mirrors.sjtug.sjtu.edu.cn/nix-channels/store"
         "https://nix-community.cachix.org"
-        "https://hercules-ci.cachix.org"
-        "https://numtide.cachix.org"
         "https://helix.cachix.org"
       ];
 
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "hercules-ci.cachix.org-1:ZZeDl9Va+xe9j+KqdzoBZMFJHVQ42Uu/c/1/KMC5Lw0="
-        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
         "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
       ];
 

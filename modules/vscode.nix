925701c2b037786aad40b15c1b354f42d7f2598e
romeoahmed <ahmedorqwn@gmail.com>
2026-01-12 22:12:23 +0800

    reflector: migrate back to vscode from fhs

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -1,21 +1,6 @@
-{ pkgs, ... }:
-
 {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscode-fhsWithPackages (
-      ps: with ps; [
-        (rust-bin.stable.latest.default.override {
-          extensions = [
-            "rust-src"
-            "rust-analyzer"
-          ];
-        })
-        clang-tools
-        lldb
-        basedpyright
-      ]
-    );
     mutableExtensionsDir = true;
   };
 }
